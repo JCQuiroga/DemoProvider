e9c8998dc235db87aa680c4be729ef21cb3ba11b
Jose C. Quiroga Garre <josecquiroga@gmail.com>
2016-04-06 11:13:26 +0000

    no message

---
+++ DemoProviderWeb/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoProviderWeb.Models;
+
+namespace DemoProviderWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        [SharePointContextFilter]
+        public ActionResult Index()
+        {
+            User spUser = null;
+
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+            var data = new List<TelefonoViewModel>();
+
+            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                if (clientContext != null)
+                {
+                    var telefonosList = clientContext.Web.Lists.GetByTitle("Telefonos");
+                    clientContext.Load(telefonosList);
+                    clientContext.ExecuteQuery();
+
+                    var query = new CamlQuery();
+                    var telefonosItems = telefonosList.GetItems(query);//Hay que pasarle la query aunq sea vacía (para no filtrar nada)
+                    clientContext.Load(telefonosItems);
+                    clientContext.ExecuteQuery();
+
+                    foreach (var telefonosItem in telefonosItems)
+                    {
+                        data.Add(TelefonoViewModel.FromListItem(telefonosItem));
+                    }
+                }
+            }
+
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DemoProviderWeb/Models/TelefonoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SharePoint.Client;
+
+namespace DemoProviderWeb.Models
+{
+    public class TelefonoViewModel
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; } 
+        public string Numero { get; set; }
+
+        public static TelefonoViewModel FromListItem(ListItem item)
+        {
+            var data = new TelefonoViewModel();
+            var id = item["ID"].ToString();
+            var ido = 0;
+            int.TryParse(id, out ido);
+            data.Id = ido;
+            data.Nombre = item["Title"].ToString(); //Ese "Title" es el nombre de la columna. Se comprueba en la ruta de enlace en el sitio web
+            data.Numero = item["Numero"].ToString(); //Ese "Title" es el nombre de la columna. Se comprueba en la ruta de enlace en el sitio web
+            return data;
+        }
+    }
+}
